4065df39e1f1c12220b55175d0a28634103f9a03
Vedant Rathi <160308562+VedanRathi11@users.noreply.github.com>
2024-03-23 13:18:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const toastBox = document.getElementById("toast-box")
+let successMsg = '<i class="fa-solid fa-circle-check"></i> Successfully Submitted';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Please fix the Error';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid Input, check again';
+
+const showToast = (msg) => {
+    let toast = document.createElement("div");
+    toast.classList.add("toast");
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
+
+    if (msg.includes("Error")) {
+        toast.classList.add("error");
+    }
+
+    if (msg.includes("Invalid")) {
+        toast.classList.add("invalid");
+    }
+
+    setTimeout(()=>{
+        toast.remove();
+    },5000)
+}
